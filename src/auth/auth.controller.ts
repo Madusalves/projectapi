7154433c85170f7b06b03877a15194d5c910a2b7
Madusalves <malvesduarte444@gmail.com>
2024-06-28 21:07:47 -0300

    commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+
+import { Controller } from '@nestjs/common';
+
+@Controller()
+
+export class AuthController{}
